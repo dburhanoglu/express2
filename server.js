7ffeb393f7b3d48af693708d2ce3af31d794a6c7
dburhanoglu <derya00burhanoglu@gmail.com>
2024-03-31 19:12:57 +0300

    htmle yönlendirme

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 const express = require('express')
 const app = express()
+
+app.set("view engine","ejs")
+
+app.get("/",(req,res) =>{
+    res.download('server.js')
+   // res.send("hiiii")
+   res.render('index')
+})
+
 app.listen(3000)
